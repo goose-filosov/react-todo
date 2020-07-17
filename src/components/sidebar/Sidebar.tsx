06321ab647b8181fa693afc2f41e889d5a62258a
goose-filosov <goose.filosov@gmail.com>
2020-07-17 22:33:46 +0700

    Создан компонент sidebar

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Sidebar.scss";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__wrapper">
+        <div className="sidebar__header"></div>
+        <div className="sidebar__body"></div>
+      </div>
+    </div>
+  );
+};
