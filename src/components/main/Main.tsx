623705531b2ee8a22cd82e4f7d25e10b68971e7f
goose-filosov <goose.filosov@gmail.com>
2020-07-18 21:50:40 +0700

    В компоненте main теперь выводятся списки задач

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import "./Main.scss";
+import { TodoList } from "components/todo-list";
 
 export const Main = () => {
   return (
     <div className="main">
       <div className="main__wrapper">
-        <Route path="/"></Route>
-        <Route path="/lists/:id"></Route>
+        <Route exact path="/">
+          {lists.map((list) => {
+            return <TodoList key={list.id} list={list} />;
+          })}
+        </Route>
+        <Route path="/lists/:id">
+          <TodoList list={lists[0]} />
+        </Route>
       </div>
     </div>
   );
