c25321d3591e7a6aea01e56ef61c2991a308d785
goose-filosov <goose.filosov@gmail.com>
2020-07-18 21:49:21 +0700

    В компонент todo-list добавлена форма добавления todo

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 import "./TodoList.scss";
 
 import { Todo } from "components/todo";
 import { ITodo } from "components/todo/Todo";
 import { IList } from "components/list/List";
+import { Button } from "components/button";
+import { AddTodo } from "components/add-todo";
 
 interface TodoListProps {
   list: IList;
@@ -12,6 +15,7 @@ interface TodoListProps {
 
 export const TodoList: React.FC<TodoListProps> = ({ list }) => {
   const { mark, name: title, todos } = list;
+  const [addFormVisible, setAddFormVisible] = useState(false);
 
   return (
     <div className="todo-list">
@@ -34,6 +38,18 @@ export const TodoList: React.FC<TodoListProps> = ({ list }) => {
                 </div>
               );
             })}
+        </div>
+        <div className="todo-list__add-item">
+          {!addFormVisible ? (
+            <Button
+              onClick={() => setAddFormVisible(!addFormVisible)}
+              theme="secondary"
+            >
+              Новая задача
+            </Button>
+          ) : (
+            <AddTodo clickHandler={() => setAddFormVisible(!addFormVisible)} />
+          )}
         </div>
       </div>
     </div>
