65ccc9f6cd3be1fe80f11aa7e87902dd10ec4d25
goose-filosov <goose.filosov@gmail.com>
2020-07-18 18:13:06 +0700

    Обновлен компонент lists, добавлен вывод списков

--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -15,30 +15,31 @@ export const Lists: React.FC<ListsProps> = ({ lists }) => {
     <nav className="lists">
       <div className="lists__wrapper">
         <ul className="lists__items">
-          {lists.map((list) => {
-            return (
-              <li className="lists__item" key={list.id}>
-                <List
-                  mark={<Mark bgColor={`${list.mark}`} />}
-                  name={list.name}
-                  to={`/lists/${list.id}`}
-                  action={
-                    <Button
-                      icon
-                      onClick={() => {
-                        console.log("Click");
-                      }}
-                    >
-                      <i
-                        className="fas fa-times"
-                        style={{ fontSize: "16px" }}
-                      />
-                    </Button>
-                  }
-                />
-              </li>
-            );
-          })}
+          {lists &&
+            lists.map((list) => {
+              return (
+                <li className="lists__item" key={list.id}>
+                  <List
+                    mark={<Mark bgColor={`${list.mark}`} />}
+                    name={list.name}
+                    to={`/lists/${list.id}`}
+                    action={
+                      <Button
+                        icon
+                        onClick={() => {
+                          console.log("Click");
+                        }}
+                      >
+                        <i
+                          className="fas fa-times"
+                          style={{ fontSize: "16px" }}
+                        />
+                      </Button>
+                    }
+                  />
+                </li>
+              );
+            })}
         </ul>
       </div>
     </nav>
