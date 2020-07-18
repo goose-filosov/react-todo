ff4b3369edf16f7dd929ecf9fa4871d86633af26
goose-filosov <goose.filosov@gmail.com>
2020-07-18 18:10:52 +0700

    Добавлены ссылки на списки в компоненте sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 import "./Sidebar.scss";
+import { Lists } from "components/lists";
+import { List } from "components/list";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__wrapper">
-        <div className="sidebar__header"></div>
-        <div className="sidebar__body"></div>
+        <div className="sidebar__header">
+          <List
+            name="Все задачи"
+            to="/"
+            mark={<i className="fas fa-list-ul" />}
+          />
+        </div>
+        <div className="sidebar__body">
+          <Lists lists={[]} />
+        </div>
       </div>
     </div>
   );
