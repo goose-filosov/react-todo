6aa9bd2308aa79e985922d44c6b7535b592834f5
goose-filosov <goose.filosov@gmail.com>
2020-07-18 21:50:02 +0700

    В компонент sidebar добавлена форма добавления списка

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Sidebar.scss";
 import { Lists } from "components/lists";
 import { List } from "components/list";
+import { Button } from "components/button";
+import { AddList } from "components/add-list";
 
 export const Sidebar = () => {
+  const [modal, setModal] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="sidebar__wrapper">
@@ -16,7 +20,18 @@ export const Sidebar = () => {
           />
         </div>
         <div className="sidebar__body">
-          <Lists lists={[]} />
+          <Lists lists={} />
+        </div>
+        <div className="sidebar__footer">
+          <Button onClick={() => setModal(!modal)}>
+            <i className="fas fa-plus" />
+            Добавить папку
+          </Button>
+          {modal && (
+            <div className="sidebar__modal">
+              <AddList clickHandler={() => setModal(!modal)} />
+            </div>
+          )}
         </div>
       </div>
     </div>
